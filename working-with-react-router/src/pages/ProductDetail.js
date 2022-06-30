deee16aaa0619674fa35157f867ca46be76ba41d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-30 15:01:51 +0000

    Extracting Route Params

--- working-with-react-router/src/pages/ProductDetail.js
+++ working-with-react-router/src/pages/ProductDetail.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
+  const params = useParams();
+
+  console.log(params.id);
   return (
     <section>
       <h1>Details about Product </h1>
+      <p>the id of this product is {params.id}</p>
     </section>
   )
 }
