c2b35c5b5188b0a121b4e7f2b388cafb25c2669b
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-30 15:16:44 +0000

    Using "Switch" and "exact" For Configuring Routes

--- working-with-react-router/src/pages/Products.js
+++ working-with-react-router/src/pages/Products.js
@@ -1,16 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>Products</h1>
 
       <ul>
-        <li>Product 1</li>
+        <li>
+          <Link to="/products/1">Product 1</Link>
+        </li>
       </ul>
       <ul>
-        <li>Product 2</li>
+        <li>
+          <Link to="products/2">Product 2</Link>
+        </li>
       </ul>
       <ul>
-        <li>Product 3</li>
+        <li>
+          <Link to="products/3">Product 3</Link>
+        </li>
       </ul>
     </section>
   );
