d45f0ebeb3c88cc26ac5d4c44f6a5dbfa7d7331c
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-30 14:58:07 +0000

    dyname=ic routes with params🛹

---
+++ working-with-react-router/src/pages/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetail = () => {
+  return (
+    <section>
+      <h1>Details about Product </h1>
+    </section>
+  )
+}
+
+export default ProductDetail
--- working-with-react-router/src/pages/Products.js
+++ working-with-react-router/src/pages/Products.js
@@ -1,5 +1,19 @@
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <section>
+      <h1>Products</h1>
+
+      <ul>
+        <li>Product 1</li>
+      </ul>
+      <ul>
+        <li>Product 2</li>
+      </ul>
+      <ul>
+        <li>Product 3</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
