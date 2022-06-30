74c4c15215a8533602bc641af7340c547d6fd2fd
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-06-30 15:23:42 +0000

    redirecting the user

--- working-with-react-router/src/App.js
+++ working-with-react-router/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import ProductDetail from "./pages/ProductDetail";
 import Products from "./pages/Products";
@@ -11,6 +11,9 @@ function App() {
 
       <main>
         <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
           <Route path="/welcome">
             <Welcome />
           </Route>
