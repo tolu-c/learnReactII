0335f3ff5df2f8c1c50db4cb3402c4fdc138d35e
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-01 15:30:07 +0000

    practicing nested routes

--- practice-project/src/pages/QuoteDetail.js
+++ practice-project/src/pages/QuoteDetail.js
@@ -1,13 +1,18 @@
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments';
 
 const QuoteDetail = () => {
   const params = useParams();
   const quoteId = params.quoteId;
-  
+
   return (
     <div>
       QuoteDetail
       <h3>This quote id is {quoteId}</h3>
+
+      <Route path={`/quotes/${quoteId}/comments`}>
+        <Comments />
+      </Route>
     </div>
   );
 };
