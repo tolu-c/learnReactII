079f240d5414d78d8e8338a9fa3eccca93b01613
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-08 02:10:54 +0000

    Adding logout

--- auth/src/components/Layout/MainNavigation.js
+++ auth/src/components/Layout/MainNavigation.js
@@ -7,6 +7,11 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  }
+
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -26,7 +31,7 @@ const MainNavigation = () => {
                 <Link to="/profile">Profile</Link>
               </li>
               <li>
-                <button>Logout</button>
+                <button onClick={logoutHandler}>Logout</button>
               </li>
             </>
           )}
