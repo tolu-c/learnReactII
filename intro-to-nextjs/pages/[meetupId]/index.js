9b012c67298213e82503913f0ee70f23e92d9389
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-11 23:28:51 +0000

    Adding Custom Components & CSS Modules

---
+++ intro-to-nextjs/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetUpDetail from "../../components/meetups/MeetUpDetail";
+
+const MeetupDetail = () => {
+  return (
+    <MeetUpDetail
+      image="https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80"
+      title="First Meetup"
+      address="Some Address"
+      description="Meetup Description"
+    />
+  );
+};
+
+export default MeetupDetail;
