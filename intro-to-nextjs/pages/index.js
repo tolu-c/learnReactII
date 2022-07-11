6c8eb133ab90f478c9091b229340e4dc7b9dcade
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-11 21:43:34 +0000

    list of meetup

---
+++ intro-to-nextjs/pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80",
+    address: "some address 4, 1236 city",
+    description: "this is the first meetup",
+  },
+  {
+    id: "m2",
+    title: "second meetup",
+    image:
+      "https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    address: "some address 4, 1236 city",
+    description: "this is the second meetup",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+};
+
+export default HomePage;
