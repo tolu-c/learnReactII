fe1c6ddc192d5402cfb2d7d39a770f51b2d3c795
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-15 15:26:58 +0000

    Introducing API Route

---
+++ intro-to-nextjs/pages/api/new-meetup.js
@@ -0,0 +1,11 @@
+// url => homepage.com/api/new-meetup
+
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+  }
+};
+
+export default handler;
