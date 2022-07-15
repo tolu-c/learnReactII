16478eb6f32818d8471f264740c89900e43844c2
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-15 16:34:50 +0000

    Working with MongoDB 👽

--- intro-to-nextjs/pages/api/new-meetup.js
+++ intro-to-nextjs/pages/api/new-meetup.js
@@ -1,10 +1,30 @@
 // url => homepage.com/api/new-meetup
 
-const handler = (req, res) => {
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://tolu:IRe3qTIZ0mBL1oEJ@intro-to-next.5axcp.mongodb.net/meetups/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({
+      status: "success",
+      message: 'Meetup created successfully',
+      data: {
+        ...data,
+        id: result.insertedId,
+      },
+    });
   }
 };
 
