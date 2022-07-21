fd82182c91cb0a8a3b2cde3606cfbf69d2ef0a8d
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-07-21 18:55:47 +0000

    formatted code.

--- animations/src/App.js
+++ animations/src/App.js
@@ -7,24 +7,26 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
-  }
+    modalIsOpen: false,
+  };
 
   showModal = () => {
-    this.setState({modalIsOpen: true});
-  }
+    this.setState({ modalIsOpen: true });
+  };
 
   closeModal = () => {
-    this.setState({modalIsOpen: false});
-  }
+    this.setState({ modalIsOpen: false });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
         <Backdrop show={this.state.modalIsOpen} />
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
